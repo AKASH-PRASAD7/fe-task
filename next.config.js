8268fa43b5fcef38700a37138687b9a770805092
AKASH PRASAD <110546856+AKASH-PRASAD7@users.noreply.github.com>
2025-07-27 21:00:05 +0530

    init

--- next.config.js
+++ next.config.js
@@ -9,12 +9,6 @@ const config = {
   images: {
     domains: ["cdn.dummyjson.com"],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default config;
